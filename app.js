e6aed15878a6f7cd212e6703dfcdf0dab30ebdd2
Ganesh Madhav Raghupathy <graghupa@usc.edu>
2021-07-17 16:37:26 -0500

    handled selection of blog and deletion v0.0.6

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const morgan = require('morgan');
 const mongoose = require('mongoose');
 
 const Blog = require('./models/blog');
+const { render } = require('ejs');
 // express app
 const app = express();
 
@@ -21,6 +22,7 @@ app.set('view engine', 'ejs');
 
 // middleware & static files
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true}));
 
 app.use((req, res, next) => {
     console.log('new request made:');
@@ -91,6 +93,42 @@ app.get('/blogs', (req, res) => {
         });
 });
 
+app.post('/blogs', (req, res) => {
+    const blog = new Blog(req.body);
+
+    blog.save()
+    .then((result) => {
+        res.redirect('/blogs');
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+});
+
+app.get('/blogs/:id', (req, res) => {
+    const id = req.params.id;
+    Blog.findById(id)
+    .then(result => {
+        res.render('details', {blog: result, title: 'Blog Details'});
+    })
+    .catch((err) => {
+        console.log(err);
+    })
+});
+
+app.delete('/blogs/:id', (req, res) => {
+    const id = req.params.id;
+
+    Blog.findByIdAndDelete(id)
+    .then(result => {
+        res.json({
+            redirect: '/blogs'
+        })
+        .catch(err => {
+            console.log(err);
+        })
+    });
+});
 app.get('/blogs/create', (req, res) => {
   res.render('create', { title: 'Create a new blog' });
 });
