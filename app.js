626c39dd2ef5bfe009c08b4c4771b3874a24320b
Ganesh Madhav Raghupathy <graghupa@usc.edu>
2021-07-17 14:50:00 -0500

    correction on morgan

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const express = require('express');
 const app = express();
 
 // morgan
-const mongan = require('morgan');
+const morgan = require('morgan');
 
 // listen for requests
 app.listen(3001);
